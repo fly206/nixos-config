e549c55ecc3557bbec81b34d8fdb1588c653bb00
fly206 <flyflyan@outlook.com>
2025-01-04 11:03:57 +0800

    update wofi theme

--- homeconf/wofi.nix
+++ homeconf/wofi.nix
@@ -13,106 +13,50 @@
       allow_markup = true;
     };
     style = ''
-      * {
-        bg0:    #2E3440F2;
-        bg1:    #3B4252;
-        bg2:    #4C566A80;
-        bg3:    #88C0D0F2;
-        fg0:    #D8DEE9;
-        fg1:    #ECEFF4;
-        fg2:    #D8DEE9;
-        fg3:    #4C566A;
-      }
+        #window {
+            background-color: rgba(45, 48, 59, 0.95);
+            border: 1px solid rgb(124, 131, 137);
+            font-family: monospace;
+            font-size: 16px;
+            color: rgb(196, 203, 212);
+        }
 
-      @import "rounded-common.rasi"
+        #outer-box {
+            padding: 5px;
+        }
 
-      element selected {
-        text-color: @bg1;
-      }
+        #input {
+            box-shadow: none;
+            background: transparent;
+            border: none;
+            border-bottom: 2px solid rgb(29, 31, 33);
+            border-radius: 0;margin-bottom: 1px;
+            padding-left: 4px;
+        }
 
-      * {
-        font:   "WenQuanYi Micro Hei 12";
+        #input:focus {
+            border: none;
+            border-bottom: 2px solid rgb(29, 31, 33);
+        }
 
-        background-color:   transparent;
-        text-color:         @fg0;
-        margin:     0px;
-        padding:    0px;
-        spacing:    0px;
-      }
+        #entry {
+            background-color: rgba(45, 48, 59, 0.95);
+            margin: 1px 0;
+            border-radius: 0;
+        }
 
-      window {
-        location:       center;
-        width:          480;
-        border-radius:  24px;
-        background-color:   @bg0;
-      }
+        #entry:nth-child(2n) {
+            background-color: rgba(64, 69, 82, 0.59);
+        }
 
-      mainbox {
-        padding:    12px;
-      }
+        #entry:selected {
+            background-color: rgb(64, 132, 214);
+            color: rgb(249, 249, 249);
+        }
 
-      inputbar {
-        background-color:   @bg1;
-        border-color:       @bg3;
-        border:         2px;
-        border-radius:  16px;
-        padding:    8px 16px;
-        spacing:    8px;
-        children:   [ prompt, entry ];
-      }
-
-      prompt {
-        text-color: @fg2;
-      }
-
-      entry {
-        placeholder:        "Search";
-        placeholder-color:  @fg3;
-      }
-
-      message {
-        margin:             12px 0 0;
-        border-radius:      16px;
-        border-color:       @bg2;
-        background-color:   @bg2;
-      }
-      textbox {
-        padding:    8px 24px;
-      }
-      listview {
-        background-color:   transparent;
-        margin:     12px 0 0;
-        lines:      8;
-        columns:    1;
-        fixed-height: false;
-      }
-
-      element {
-        padding:        8px 16px;
-        spacing:        8px;
-        border-radius:  16px;
-      }
-
-      element normal active {
-        text-color: @bg3;
-      }
-
-      element alternate active {
-        text-color: @bg3;
-      }
-
-      element selected normal, element selected active {
-        background-color:   @bg3;
-      }
-
-      element-icon {
-        size:           1em;
-        vertical-align: 0.5;
-      }
-
-      element-text {
-        text-color: inherit;
-      }
+        #img {
+            margin: -1px 8px -1px 0;
+        }
     '';
   };
 }
