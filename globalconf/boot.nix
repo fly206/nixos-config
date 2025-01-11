1adc6c62f1ddef5a6fd972f5a6477d98024b73ac
fly206 <flyflyan@outlook.com>
2025-01-11 13:03:57 +0800

    remove refind

--- globalconf/boot.nix
+++ globalconf/boot.nix
@@ -2,15 +2,8 @@
 
 {
   boot.loader = {
-    systemd-boot.enable = false; # 确保 systemd-boot 被禁用
+    systemd-boot.enable = true; # 确保 systemd-boot 被禁用
     grub.enable = false;         # 确保 GRUB 被禁用
-    refind = {
-      enable = true;
-      useNvram = false; # 如果你的系统支持 NVRAM
-      # extraConfig 可以用于添加额外的 rEFInd 配置
-      extraConfig = ''
-        timeout 20
-      '';
     };
   };
 }
