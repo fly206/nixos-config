52d30fe1c4ac58857b676632a6eb3bef4ce17bb0
fly206 <flyflyan@outlook.com>
2025-01-14 21:10:42 +0800

    add vscode

--- homeconf/pkgs/vscode.nix
+++ homeconf/pkgs/vscode.nix
@@ -8,7 +8,7 @@
     extensions = [
         pkgs.vscode-extensions.bbenoist.nix
         pkgs.vscode-extensions.ms-vscode.cmake-tools
-        pkgs.vscode-extensions.ms-vscode.cpptools
+        # pkgs.vscode-extensions.ms-vscode.cpptools
         pkgs.vscode-extensions.ms-vscode.makefile-tools
     ];
   };
